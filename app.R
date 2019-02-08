8053e16b25b714320cc14382c193378763b9130c
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2019-02-08 12:53:32 -0500

    Add icon and metadata

--- app.R
+++ app.R
@@ -43,6 +43,34 @@ ui <- cartridge(
     }
     "
   )),
+  tags$head(HTML(
+  '
+  <title>Trump Twitter Time!</title>
+  <meta property="og:title" content="Trump Twitter Time!">
+  <meta property="og:description" content="A simple game for your Executive Time">
+  <meta property="og:url" content="https://apps.garrickadenbuie.com">
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:creator" content="@grrrck">
+  <meta name="twitter:site" content="https://garrickadenbuie.com">
+  <link rel="apple-touch-icon" sizes="57x57" href="apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192"  href="android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+  <link rel="manifest" href="manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  ')),
   container_with_title(
     title = "When did Trump tweet...?",
     uiOutput("tweet_balloon")
